6281a7a48d6ebe80fc63b49a4c9690105a99f78f
asilva-juarez <asilva-juarez@myseneca.ca>
2024-01-21 15:07:14 -0500

    05 Solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  // we need to know what is the bigger number
+  let greaterNum = 0;
+  let lessNum = 0;
+  let sum = 0;
 
+  if (a > b) {
+    greaterNum = a;
+    lessNum = b;
+  } else {
+    greaterNum = b;
+    lessNum = a;
+  }
+
+  // Sum the range until the greaterNum reach the less number
+  while (greaterNum !== lessNum) {
+    sum += greaterNum;
+    greaterNum--;
+  }
+
+  // when it reach the less number it does not sum the reached number, we need to add it too
+  sum += lessNum;
+
+  return sum;
 };
 
 // Do not edit below this line
