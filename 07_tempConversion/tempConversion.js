8b81b6c353dda243bdfa02ae713d3040aae7e798
asilva-juarez <asilva-juarez@myseneca.ca>
2024-01-21 15:17:17 -0500

    07 Solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (num) {
+  // FORMULA
+  // (ºF - 32) / 1.8
+  return Math.round((num - 32) / 1.8) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (num) {
+  // FORMULA
+  // (ºC * 1.8) + 32
+  return Math.round(num * 1.8 + 32) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
