4ca3b5bd84c4babb804ea3f31eb1cc7dac11d8e7
asilva-juarez <asilva-juarez@myseneca.ca>
2024-01-22 00:28:20 -0500

    12 Solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const person = people.reduce((oldest, current) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
 
+    const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+
+    return oldestAge < currentAge || !oldestAge ? current : oldest;
+  });
+
+  return person;
+};
+
+const getAge = (birth, death) => {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+
+  return death - birth;
 };
 
 // Do not edit below this line
