a06c4efa9b4789d8ba1e42ffb306b6ed6ec3ca7c
asilva-juarez <asilva-juarez@myseneca.ca>
2024-01-22 00:03:35 -0500

    10 Solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+  // Do the sequence until the index given
+  const fibonacciSeq = new Array();
+  for (let i = 0; i < index; i++) {
+    if (i === 0 || i === 1) {
+      fibonacciSeq.push(1);
+    } else {
+      fibonacciSeq.push(fibonacciSeq[i - 1] + fibonacciSeq[i - 2]);
+    }
+  }
 
+  return fibonacciSeq[index - 1];
 };
 
 // Do not edit below this line
