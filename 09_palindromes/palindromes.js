6a348fc7d2febec12eb71232ef81d130e487c5d1
asilva-juarez <asilva-juarez@myseneca.ca>
2024-01-21 23:53:19 -0500

    09 Solve

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const original = str;
+  const reverse = str.split("").reverse();
+  let isPalindrome = true;
 
+  for (let i = 0; i < str.length && isPalindrome; i++) {
+    if (original[i] !== reverse[i]) isPalindrome = false;
+  }
+
+  return isPalindrome;
 };
 
 // Do not edit below this line
