c85b48e1498ffb6e79674e3945f85c34b34ca4e0
asilva-juarez <asilva-juarez@myseneca.ca>
2024-01-21 23:49:08 -0500

    08 Solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((acc, val) => (acc += val), 0);
 };
 
-const multiply = function() {
-
+const multiply = function (a, b) {
+  return a * b;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  let result = 1;
+  while (b === 0) {
+    result *= a;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a !== 1) return factorial(a--);
 };
 
 // Do not edit below this line
@@ -29,5 +34,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
